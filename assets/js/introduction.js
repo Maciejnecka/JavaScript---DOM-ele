2c5f8f937114fed198a53513979b614596096e00
Maciej Necka <macieknecka@gmail.com>
2023-09-29 09:35:52 +0200

    introduction.js - createTooltip>Links>Text>Image added

--- assets/js/introduction.js
+++ assets/js/introduction.js
@@ -8,22 +8,38 @@ tooltips.forEach((tooltips) => {
   const tooltipType = tooltips.dataset.tooltipType;
   const tooltipContent = tooltips.dataset.tooltipContent;
 
-  const linkElement = document.createElement('a');
-  linkElement.setAttribute('href', `${url}`);
-  linkElement.textContent = ' ' + tooltips.textContent;
+  const linksElement = createTooltipLinks(url, tooltips);
 
+  const spanElement = createTooltipText(tooltipType, tooltipContent);
+
+  if (tooltipType === 'image') {
+    // createTooptipImage
+    const imgElement = createTopptipImage(linkSrc);
+    spanElement.textContent = '';
+    spanElement.appendChild(imgElement);
+  }
+  tooltips.appendChild(linksElement);
+  tooltips.appendChild(spanElement);
+});
+
+function createTooltipLinks(url, tooltips) {
+  const linksElement = document.createElement('a');
+  linksElement.setAttribute('href', url);
+  linksElement.textContent = ' ' + tooltips.textContent;
+  return linksElement;
+}
+
+function createTooltipText(tooltipType, tooltipContent) {
   const spanElement = document.createElement('span');
   spanElement.classList.add('tooltip__box', `tooltip__box--${tooltipType}`);
   spanElement.textContent = tooltipContent;
   tooltips.textContent = '';
-  if (tooltipType === 'image') {
-    const imgElement = document.createElement('img');
-    imgElement.setAttribute('src', `${linkSrc}`);
-    imgElement.classList.add('tooltip__image');
-    spanElement.textContent = '';
-    spanElement.appendChild(imgElement);
-  }
+  return spanElement;
+}
 
-  tooltips.appendChild(linkElement);
-  tooltips.appendChild(spanElement);
-});
+function createTopptipImage(linkSrc) {
+  const imgElement = document.createElement('img');
+  imgElement.setAttribute('src', linkSrc);
+  imgElement.classList.add('tooltip__image');
+  return imgElement;
+}
